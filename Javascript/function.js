d6f766196765191959b9d6f0c171f273193c1e38
JungSeongCheol <kir5121@naver.com>
2020-10-31 09:23:59 +0900

    function revision

--- Javascript/function.js
+++ Javascript/function.js
@@ -9,7 +9,6 @@
 // naming: doSomething, command, verb
 // e.g. createCardAndPoint -> createCard, createPoint
 // function is object in JS
-
 function printHello(){
     console.log('Hello');
 }
@@ -31,3 +30,29 @@ changeName(Jung);
 console.log(Jung);
 
 // 3.Default parameters (added in ES6)
+function showMessage(message, from = 'unknown'){
+    console.log(`${message} by ${from}`);
+}
+showMessage('Hi!');
+
+// 4. Rest parameters (added in ES6)
+function printAll(...args){
+    for(let i = 0; i < args.length; i++){
+        console.log(args[i]);
+    }
+
+    for (const arg of args) {
+        console.log(arg);
+    }
+
+    args.forEach((arg) => console.log(arg))
+}
+printAll('dream', 'coding', 'Jung');
+
+// 5. Local scope
+let globalMessage = 'global'; // global variable
+function printMessage() {
+    let message = 'hello';
+    console.log(message);
+    console.log(globalMessage);
+}
