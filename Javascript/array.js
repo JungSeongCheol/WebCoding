34b48af598e3fd352f67158a678233fd5cb928e8
JungSeongCheol <kir5121@naver.com>
2020-11-06 23:47:16 +0900

    using array in javascript

--- Javascript/array.js
+++ Javascript/array.js
@@ -10,3 +10,23 @@ const arr2 = [1, 2];
 const fruits = ['🍎', '🍌'];
 console.log(fruits);
 console.log(fruits.length);
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[fruits.length - 1]);
+
+console.clear();
+// 3. Looping over an array
+// print all fruits
+// a. for
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+// b. for of const or let
+for (const iterator of fruits) {
+    console.log(iterator)
+}
+
+// c. forEach
+fruits.forEach((fruit) => console.log(fruit));
