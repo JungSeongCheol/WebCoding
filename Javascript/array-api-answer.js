d0006b0a9876a5786d305f7baa5d0de1200944cd
JungSeongCheol <kir5121@naver.com>
2020-11-09 23:30:51 +0900

    array Api Question

---
+++ Javascript/array-api-answer.js
@@ -0,0 +1,11 @@
+// Q1. make a string out of an array
+{
+    const fruits = ['apple', 'banana', 'orange'];
+    const array = fruits.join(',');
+    console.log(array);
+}
+
+// Q2. make an array out of a string
+{
+    const fruits = ''
+}
---
+++ Javascript/array-api.js
@@ -0,0 +1,12 @@
+// Q1. make a string out of an array
+{
+    const fruits = ['apple', 'banana', 'orange'];
+    const array = fruits.toString();
+    console.log(array);
+    console.log(typeof array);
+}
+
+// Q2. make an array out of a string
+{
+    const fruits = ''
+}
