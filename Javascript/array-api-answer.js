7dba005e86bdbd3f3e4b7c92a730d493cb2b21c6
JungSeongCheol <kir5121@naver.com>
2020-11-09 23:52:04 +0900

    Array Api

--- Javascript/array-api-answer.js
+++ Javascript/array-api-answer.js
@@ -7,5 +7,24 @@
 
 // Q2. make an array out of a string
 {
-    const fruits = ''
+    const fruits = '🍎, 🥝, 🍌, 🍒';
+    const array = fruits.split(',');
+    console.log(array);
+}
+
+// Q3. make this array look like this: [5, 4, 3, 2, 1]
+{
+    const array = [1, 2, 3, 4, 5];
+    const result = array.reverse();
+    console.log(result);
+    console.log(array);
+}
+
+// Q4. make new array without the first two elements
+// make - splice 사용 불가능
+{
+    const array = [1, 2, 3, 4, 5];
+    const result = array.slice(2, 5); // slice는 array 에 변동은 주지 않음(splice와 다르게)
+    console.log(result);
+    console.log(array);
 }
--- Javascript/array-api.js
+++ Javascript/array-api.js
@@ -8,5 +8,22 @@
 
 // Q2. make an array out of a string
 {
-    const fruits = ''
+    const fruits = '🍎, 🥝, 🍌, 🍒';
+    const array = fruits.split(',');
+    console.log(array);
+}
+
+// Q3. make this array look like this: [5, 4, 3, 2, 1]
+{
+    const array = [1, 2, 3, 4, 5];
+    const result = array.reverse();
+    console.log(result);
+}
+
+// Q4. make new array without the first two elements
+{
+    const array = [1, 2, 3, 4, 5];
+    const result = array.slice(2, 5);
+    console.log(result);
+    console.log(array);
 }
