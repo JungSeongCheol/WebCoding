e8d8c4d339f468740ee4faf722c338f3fb591940
JungSeongCheol <kir5121@naver.com>
2020-11-10 00:14:26 +0900

    Array Api

--- Javascript/array-api.js
+++ Javascript/array-api.js
@@ -27,3 +27,29 @@
     console.log(result);
     console.log(array);
 }
+
+class Student {
+    constructor(name, age, enrolled, score) {
+        this.name = name;
+        this.age = age;
+        this.enrolled = enrolled;
+        this.score = score;
+    }
+}
+
+const students = [
+    new Student('A', 29, true, 45),
+    new Student('B', 28, false, 80),
+    new Student('C', 30, true, 90),
+    new Student('D', 40, false, 66),
+    new Student('E', 18, true, 88)
+];
+
+// Q5. find a student with the score 90
+{
+    for (const iterator of students) {
+        if(iterator.score === 90) console.log(iterator.name);
+    }
+}
+
+// 06. make an array of enrolled students
