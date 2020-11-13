8ae6a217afdba35c6a5c951c0a57b05090548627
JungSeongCheol <kir5121@naver.com>
2020-11-13 23:29:07 +0900

    Callback Tip JavaScript

---
+++ Javascript/async/callback.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// JavaScript is synchronous
+// Execute the code block by orger after hositing.
+// hoisting: var, function declaration
+console.log('1');
+setTimeout();
+console.log('2');
+console.log('3');
