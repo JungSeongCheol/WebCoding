453897a3f5ec5561b1b4ff12f6f0a9714317782f
JungSeongCheol <kir5121@naver.com>
2020-11-13 23:38:39 +0900

    Async Callback.js

--- Javascript/async/callback.js
+++ Javascript/async/callback.js
@@ -1,9 +1,22 @@
 'use strict';
 
+function printImmediately(print) {
+    print();
+}
 // JavaScript is synchronous
-// Execute the code block by orger after hositing.
+// Execute the code block in orger after hositing.
 // hoisting: var, function declaration
 console.log('1');
-setTimeout();
-console.log('2');
+setTimeout(() => console.log('2'), 1000);
 console.log('3');
+
+// Synchronous callback
+
+printImmediately(() => console.log('hello'));
+
+// Asynchronous callback
+function printWithDelay(print, timeout) {
+    setTimeout(print, timeout);
+}
+
+printWithDelay(() => console.log('async callback'), 2000);
